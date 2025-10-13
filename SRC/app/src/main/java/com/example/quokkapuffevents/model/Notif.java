78792d60b539a212b5db5441353c6eae38cd0ef6
Seth Close <seth@sethclose.au>
2025-10-13 01:24:12 -0600

    Created model framework and connected firebase

---
+++ SRC/app/src/main/java/com/example/quokkapuffevents/model/Notif.java
@@ -0,0 +1,11 @@
+package com.example.quokkapuffevents.model;
+
+public class Notif {
+    private Integer id;
+    private Integer type; //If just a notification or a choice
+    private User recipient;
+    private Event originEvent;
+    private User originUser;
+    private String Message;
+
+}
