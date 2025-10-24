2bc08ed1d2bf13ee45456ca5009677316608872c
Seth Close <seth@sethclose.au>
2025-10-24 16:24:00 -0600

    Made create user/event methods and methods to return lists of events or users

--- SRC/app/src/main/java/com/example/quokkapuffevents/model/Database.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/model/Database.java
@@ -1,9 +1,24 @@
 package com.example.quokkapuffevents.model;
 
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+import static android.content.ContentValues.TAG;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public class Database {
@@ -51,5 +66,91 @@ public class Database {
         return(newEvent);
     }
 
-    
+    public ArrayList<Event> listEvents(){
+        //Got the basis of this code from the firebase website: https://firebase.google.com/docs/firestore/query-data/get-data
+       ArrayList<Event> events = new ArrayList<>();
+
+       //Will return every single event
+       eventsRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                events.add(document.toObject(Event.class));
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+       return events;
+    }
+
+    public ArrayList<User> usersInEvent(Event event){
+        //Got the basis of this code from the firebase website: https://firebase.google.com/docs/firestore/query-data/get-data
+        ArrayList<User> users = new ArrayList<>(); //because I cannot create or access within the mini function
+        ArrayList<Event> specificEvent = new ArrayList<>(); //because I cannot create or access within the mini function
+
+        eventsRef.document(event.getId()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                specificEvent.set(0, documentSnapshot.toObject(Event.class));
+            }
+        });
+
+        List usersInEvent = (List) specificEvent.get(0).getEventUsers().keySet();
+
+        usersRef.whereIn("id", usersInEvent)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                users.add(document.toObject(User.class));
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+        return(users);
+    }
+
+    //public ArrayList<User> drawUsers
+
+    //public ArrayList<User> redrawUsers
+
+    //public void deleteUser
+
+    //public void deleteEvent
+
+    //public ArrayList<Event> getEventsFromUser
+
+    public User getUser(String userID){
+        //Got the basis of this code from the firebase website: https://firebase.google.com/docs/firestore/query-data/get-data
+        ArrayList<User> user = new ArrayList<>(); //because I cannot create or access within the mini function
+
+        usersRef.document(userID).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                user.set(0, documentSnapshot.toObject(User.class));
+            }
+        });
+        return(user.get(0));
+    }
+
+    public Event getEvent(String eventID){
+        //Got the basis of this code from the firebase website: https://firebase.google.com/docs/firestore/query-data/get-data
+        ArrayList<Event> event = new ArrayList<>(); //because I cannot create or access within the mini function
+
+        eventsRef.document(eventID).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                event.set(0, documentSnapshot.toObject(Event.class));
+            }
+        });
+        return(event.get(0));
+    }
 }
