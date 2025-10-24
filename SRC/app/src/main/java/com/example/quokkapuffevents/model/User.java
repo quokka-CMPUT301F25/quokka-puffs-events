892023fe5f4682e62af3942f2887cb8bca3d0880
Seth Close <seth@sethclose.au>
2025-10-24 14:19:10 -0600

    Updated User and Events variables and constructors to better match plan

--- SRC/app/src/main/java/com/example/quokkapuffevents/model/User.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/model/User.java
@@ -1,16 +1,24 @@
 package com.example.quokkapuffevents.model;
 
+import java.util.ArrayList;
+
 public class User {
     private String id;
     private String email;
+    private String hashPassword;
+    private String userName;
+    private ArrayList<String> events;
     private Integer accountType; //-'ve Admin, 0 for normal user, +'ve for organiser
     //etc etc
 
 
 
-    public User(String id, String email, Integer accountType){
+    public User(String id, String email, Integer accountType, String hashPassword, String userName){
         this.id = id;
         this.email = email;
+        this.hashPassword = hashPassword;
+        this.userName = userName;
+        this.events = new ArrayList();
         this.accountType = accountType;
     }
 
@@ -25,4 +33,43 @@ public class User {
     public String getId() {
         return id;
     }
+
+    public ArrayList<String> getEvents() {
+        return events;
+    }
+
+    public String getHashPassword() {
+        return hashPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setAccountType(Integer accountType) {
+        this.accountType = accountType;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setEvents(ArrayList<String> events) {
+        this.events = events;
+    }
+
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
 }
