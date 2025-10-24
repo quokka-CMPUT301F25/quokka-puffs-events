dd5a8a59463e0c817fdd86b19d102fc761f0e273
Seth Close <seth@sethclose.au>
2025-10-24 14:19:10 -0600

    Updated User and Events variables and constructors to better match plan

--- SRC/app/src/main/java/com/example/quokkapuffevents/model/Database.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/model/Database.java
@@ -3,6 +3,9 @@ package com.example.quokkapuffevents.model;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Date;
+import java.util.Optional;
+
 public class Database {
     private static volatile Database instance;
     private Integer userID; //current id of the user of the app
@@ -34,10 +37,19 @@ public class Database {
     }
 
 
-    public User createUser(String email, Integer type){
+    public User createUser(String email, Integer type, String hashPass, String userName){
         String id = usersRef.document().getId(); //Creates a document and returns the id
-        User newUser = new User(id, email, type);
+        User newUser = new User(id, email, type, hashPass, userName);
         usersRef.document(id).set(newUser);
         return(newUser);
     }
+
+    public Event createEvent(String idEvent, User org, String description, Integer toBeDrawn, Optional<Integer> maxNumWaitlist, Date startDate, Date endDate){
+        String id = eventsRef.document().getId(); //Creates a document and returns the id
+        Event newEvent = new Event(idEvent, org.getId(), description, toBeDrawn, maxNumWaitlist, startDate, endDate);
+        eventsRef.document(id).set(newEvent);
+        return(newEvent);
+    }
+
+    
 }
