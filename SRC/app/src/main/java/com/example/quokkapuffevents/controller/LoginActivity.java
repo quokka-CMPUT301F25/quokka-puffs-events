1348f2a3e2d06ab5628311bd9104f4bd07dc2a29
Aaron Bedard <abedard2005@gmail.com>
2025-11-03 11:35:44 -0700

    Premerge push

--- SRC/app/src/main/java/com/example/quokkapuffevents/controller/LoginActivity.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/controller/LoginActivity.java
@@ -1,4 +1,44 @@
 package com.example.quokkapuffevents.controller;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quokkapuffevents.R;
+import com.example.quokkapuffevents.model.Database;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Database db;
+
+    public static final String PREFS_NAME = "RemeberMePrefs";
+    private static final String PREF_USERID = "userID";
+    private static final String PREF_PASSWORD = "password";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.login_page);
+
+        db = Database.getInstance();
+
+
+
+        EditText username = findViewById(R.id.login_email_address);
+        EditText password = findViewById(R.id.login_password);
+        CheckBox rememberMe = findViewById(R.id.remember_me_button);
+
+        String uNameOrEmail = username.getText().toString();
+        String pass = password.getText().toString();
+        boolean remember = rememberMe.isChecked();
+
+        
+
+
+
+    }
 }
