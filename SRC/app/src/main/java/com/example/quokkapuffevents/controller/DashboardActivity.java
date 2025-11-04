92bac7909bcf08f8570e9d06385e952006370770
Aaron Bedard <abedard2005@gmail.com>
2025-11-03 18:43:14 -0700

    Merging fixes to ValidataeUserUsername:

--- SRC/app/src/main/java/com/example/quokkapuffevents/controller/DashboardActivity.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/controller/DashboardActivity.java
@@ -1,4 +1,18 @@
 package com.example.quokkapuffevents.controller;
 
-public class DashboardActivity {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quokkapuffevents.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.main_dashboard);
+    }
 }
--- SRC/app/src/main/java/com/example/quokkapuffevents/controller/LoginActivity.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/controller/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.example.quokkapuffevents.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,19 +30,89 @@ public class LoginActivity extends AppCompatActivity {
 
         db = Database.getInstance();
 
-
-
         EditText username = findViewById(R.id.login_email_address);
         EditText password = findViewById(R.id.login_password);
         CheckBox rememberMe = findViewById(R.id.remember_me_button);
+        Button signInButton = findViewById(R.id.sign_in_button);
 
-        String uNameOrEmail = username.getText().toString();
-        String pass = password.getText().toString();
-        boolean remember = rememberMe.isChecked();
+        signInButton.setOnClickListener(v -> {
 
-        
+            String uNameOrEmail = username.getText().toString();
+            String pass = password.getText().toString();
+            boolean remember = rememberMe.isChecked();
 
+            db.UserExists(uNameOrEmail, userExists -> {
+                if(userExists) {
+                    if(uNameOrEmail.contains("@")){
+                        db.ValidateUserEmail(uNameOrEmail, pass, users -> {
+                            if (!users.isEmpty()) {
+                                db.SetUserID(users.get(0).getId());
+                                SwitchActivity(DashboardActivity.class);
+                            }
+                            else {
+                                DisplayErrorMsg();
+                            }
+                        });
+                    }
+                    else {
+                        db.ValidateUserUsername(uNameOrEmail, pass, users -> {
+                            System.out.println(users.size());
+                            if (!users.isEmpty()) {
+                                db.SetUserID(users.get(0).getId());
+                                SwitchActivity(DashboardActivity.class);
+                            }
+                            else {
+                                DisplayErrorMsg();
+                            }
+                        });
+                    }
+                }
+                else {
+                    DisplayErrorMsg();
+                }
+            });
+        });
+    }
 
+    private void SwitchActivity(Class<?> activity){
+        Intent intent = new Intent(this, activity);
+        startActivity(intent);
+    }
 
+    private void DisplayErrorMsg(){
+        Toast.makeText(this, "Username or Password is incorrect", Toast.LENGTH_SHORT).show();
+    }
+    private void ValidateInformation(String uNameOrEmail,  String pass){
+        db.UserExists(uNameOrEmail, userExists -> {
+            if(userExists) {
+                if(uNameOrEmail.contains("@")){
+                    db.ValidateUserEmail(uNameOrEmail, pass, users -> {
+                        if (!users.isEmpty()) {
+                            db.SetUserID(users.get(0).getId());
+                            SwitchActivity(DashboardActivity.class);
+                        }
+                        else {
+                            DisplayErrorMsg();
+                        }
+                    });
+                }
+                else {
+                    db.ValidateUserUsername(uNameOrEmail, pass, users -> {
+                        if (!users.isEmpty()) {
+                            db.SetUserID(users.get(0).getId());
+                            SwitchActivity(DashboardActivity.class);
+                        }
+                        else {
+                            DisplayErrorMsg();
+                        }
+                    });
+                }
+            }
+            else {
+                DisplayErrorMsg();
+            }
+        });
     }
 }
+
+
