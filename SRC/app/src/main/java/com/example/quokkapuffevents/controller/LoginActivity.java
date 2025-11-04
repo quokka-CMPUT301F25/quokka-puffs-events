ec46ae0dce34889cc5f13dc717142470b9583562
Aaron Bedard <abedard2005@gmail.com>
2025-11-03 19:12:40 -0700

    Can now login

--- SRC/app/src/main/java/com/example/quokkapuffevents/controller/LoginActivity.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/controller/LoginActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.content.SharedPreferences;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +18,8 @@ import com.example.quokkapuffevents.model.Database;
 public class LoginActivity extends AppCompatActivity {
 
     private Database db;
+    private SharedPreferences.Editor loginPrefsEditor;
 
-    public static final String PREFS_NAME = "RemeberMePrefs";
-    private static final String PREF_USERID = "userID";
-    private static final String PREF_PASSWORD = "password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,78 +29,83 @@ public class LoginActivity extends AppCompatActivity {
 
         db = Database.getInstance();
 
+        //--UI BINDING--
         EditText username = findViewById(R.id.login_email_address);
         EditText password = findViewById(R.id.login_password);
         CheckBox rememberMe = findViewById(R.id.remember_me_button);
         Button signInButton = findViewById(R.id.sign_in_button);
 
+        //Setting up login preferences for "remember me" option
+        SharedPreferences loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+
+        //If the user has previously logged in, and has selected 'Remember me" fill Username and password
+        Boolean saveLogin = loginPreferences.getBoolean("saveLogin", false);
+        if (saveLogin) {
+            username.setText(loginPreferences.getString("username", ""));
+            password.setText(loginPreferences.getString("password", ""));
+            rememberMe.setChecked(true);
+        }
+
         signInButton.setOnClickListener(v -> {
+            ValidateInformation(username.getText().toString(), password.getText().toString());
 
-            String uNameOrEmail = username.getText().toString();
-            String pass = password.getText().toString();
-            boolean remember = rememberMe.isChecked();
+            //Store login information in SharedPreferences if `Remember me` is checked
+            if (rememberMe.isChecked()) {
+                loginPrefsEditor.putBoolean("saveLogin", true);
+                loginPrefsEditor.putString("username", username.getText().toString());
+                loginPrefsEditor.putString("password", password.getText().toString());
+                loginPrefsEditor.commit();
+            } else {
+                //Clear the data if they uncheck it?
 
-            db.UserExists(uNameOrEmail, userExists -> {
-                if(userExists) {
-                    if(uNameOrEmail.contains("@")){
-                        db.ValidateUserEmail(uNameOrEmail, pass, users -> {
-                            if (!users.isEmpty()) {
-                                db.SetUserID(users.get(0).getId());
-                                SwitchActivity(DashboardActivity.class);
-                            }
-                            else {
-                                DisplayErrorMsg();
-                            }
-                        });
-                    }
-                    else {
-                        db.ValidateUserUsername(uNameOrEmail, pass, users -> {
-                            System.out.println(users.size());
-                            if (!users.isEmpty()) {
-                                db.SetUserID(users.get(0).getId());
-                                SwitchActivity(DashboardActivity.class);
-                            }
-                            else {
-                                DisplayErrorMsg();
-                            }
-                        });
-                    }
-                }
-                else {
-                    DisplayErrorMsg();
-                }
-            });
+                //TODO: Decide if we want to do this or not/if we to even open the activity
+                loginPrefsEditor.clear();
+                loginPrefsEditor.commit();
+            }
         });
     }
 
+    //Changing activity depending on what the user has selected
+    // Note the `Class<?>` means it can be any class
     private void SwitchActivity(Class<?> activity){
         Intent intent = new Intent(this, activity);
         startActivity(intent);
     }
 
+    //Display's if the Username or password is incorrect
     private void DisplayErrorMsg(){
         Toast.makeText(this, "Username or Password is incorrect", Toast.LENGTH_SHORT).show();
     }
+
+    //Validates the information entered by the user
     private void ValidateInformation(String uNameOrEmail,  String pass){
         db.UserExists(uNameOrEmail, userExists -> {
             if(userExists) {
+                //If the user has typed in an email, check it
                 if(uNameOrEmail.contains("@")){
                     db.ValidateUserEmail(uNameOrEmail, pass, users -> {
                         if (!users.isEmpty()) {
+                            //Set the current user in the database and change activity
                             db.SetUserID(users.get(0).getId());
                             SwitchActivity(DashboardActivity.class);
                         }
+                        //If password is wrong then we have an error
                         else {
                             DisplayErrorMsg();
                         }
                     });
                 }
+                //If the user has typed in a username, check it
                 else {
                     db.ValidateUserUsername(uNameOrEmail, pass, users -> {
                         if (!users.isEmpty()) {
+                            //Set the current user in the database and change activity
                             db.SetUserID(users.get(0).getId());
                             SwitchActivity(DashboardActivity.class);
+
                         }
+                        //If no user is found then we have an error
                         else {
                             DisplayErrorMsg();
                         }
