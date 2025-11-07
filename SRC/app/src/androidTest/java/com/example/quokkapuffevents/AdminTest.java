e4fdbae38a2f01c9d0941e177b70e3c03b4f9efd
Kishan <prasadkishan2@outlook.com>
2025-11-07 01:56:40 -0700

    Finished Admin Tests

--- SRC/app/src/androidTest/java/com/example/quokkapuffevents/AdminTest.java
+++ SRC/app/src/androidTest/java/com/example/quokkapuffevents/AdminTest.java
@@ -1,40 +1,21 @@
 package com.example.quokkapuffevents;
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.intent.Intents.*;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.*;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.hasProperty;
-import static java.util.EnumSet.allOf;
-
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.example.quokkapuffevents.controller.AdminActivity;
 import com.example.quokkapuffevents.controller.LoginActivity;
-import com.example.quokkapuffevents.model.Event;
 import com.example.quokkapuffevents.model.User;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-
-import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import com.example.quokkapuffevents.model.Database;
@@ -42,9 +23,6 @@ import com.example.quokkapuffevents.model.Database;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Date;
-import java.util.Locale;
-
 
 @RunWith(AndroidJUnit4.class)
 public class AdminTest {
