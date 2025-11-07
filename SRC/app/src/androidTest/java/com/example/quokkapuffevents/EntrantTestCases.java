b89c6f9fb6f8f2bf2246998896bfe929783036ba
Kishan <prasadkishan2@outlook.com>
2025-11-07 11:08:54 -0700

    File Setup

--- SRC/app/src/androidTest/java/com/example/quokkapuffevents/EntrantTestCases.java
+++ SRC/app/src/androidTest/java/com/example/quokkapuffevents/EntrantTestCases.java
@@ -1,4 +1,60 @@
 package com.example.quokkapuffevents;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.quokkapuffevents.controller.LoginActivity;
+import com.example.quokkapuffevents.model.Database;
+import com.example.quokkapuffevents.model.User;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@RunWith(AndroidJUnit4.class)
 public class EntrantTestCases {
+    Database db = Database.getInstance();
+
+    public User createMockEntrant() {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        byte[] hashedPasswordByte = md.digest("password".getBytes(StandardCharsets.UTF_8));
+        String hashedPassword = new String(hashedPasswordByte);
+        User user = db.CreateUser("TestingAdmin@Admin.ca", 0, hashedPassword,
+                "TestingAdmin", "FirstAdmin", "LastAdmin",
+                "5871234567");
+        return user;
+    }
+
+    public void accessEntrantDashboard() {
+        User mockAdmin = createMockEntrant();
+        try (ActivityScenario<LoginActivity> scenario =
+                     ActivityScenario.launch(LoginActivity.class)) {
+
+            onView(withId(R.id.login_email_address)).perform(typeText("TestingAdmin"));
+            onView(withId(R.id.login_password)).perform(typeText("password"));
+            onView(withId(R.id.sign_in_button)).perform(click());
+
+            Thread.sleep(1500);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        deleteMockAdmin(mockAdmin);
+
 }
--- SRC/app/src/androidTest/java/com/example/quokkapuffevents/OrganizerTestCases.java
+++ SRC/app/src/androidTest/java/com/example/quokkapuffevents/OrganizerTestCases.java
@@ -1,4 +1,27 @@
 package com.example.quokkapuffevents;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.quokkapuffevents.controller.LoginActivity;
+import com.example.quokkapuffevents.model.Database;
+import com.example.quokkapuffevents.model.User;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@RunWith(AndroidJUnit4.class)
 public class OrganizerTestCases {
+
 }
