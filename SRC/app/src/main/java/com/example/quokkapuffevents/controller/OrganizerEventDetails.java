3337f03171b9ffc164939442f0aca24db945377c
Aaron Bedard <abedard2005@gmail.com>
2025-11-06 22:06:12 -0700

    Can now draw users for event

--- SRC/app/src/main/java/com/example/quokkapuffevents/controller/OrganizerEventDetails.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/controller/OrganizerEventDetails.java
@@ -19,34 +19,38 @@ public class OrganizerEventDetails extends Fragment {
     Database db;
     Event event;
 
-    Button drawUsers;
-    Button viewParticipants;
-    Button changeDetails;
+    Button runLottoButton;
+    Button viewParticipantsButton;
+    Button changeDetailsButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout."NAME", container, false);
+        return inflater.inflate(R.layout.organizer_event_dashboard, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        initialize();
+        initialize(view);
 
-        drawUsers.setOnClickListener(v -> {
+        runLottoButton.setOnClickListener(v -> {
             event.drawUsers(event.getToBeDrawn());
             event.setDrawn(true);
+            db.SaveEvent(event);
         });
     }
 
-    private void initialize() {
+    private void initialize(View view) {
         db = Database.getInstance();
 
         //Button Shit
+        runLottoButton = view.findViewById(R.id.orgRunLotteryBtn);
+        viewParticipantsButton = view.findViewById(R.id.orgViewParticipantsBtn);
+        changeDetailsButton = view.findViewById(R.id.orgChangeDetailsBtn);
     }
 
     public void SetEvent(Event event) {this.event = event; }
