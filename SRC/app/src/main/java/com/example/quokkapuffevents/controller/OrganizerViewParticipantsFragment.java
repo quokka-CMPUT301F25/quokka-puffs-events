0ad26c3cb603259655d16a77d025d8d91b88de09
kylemnguyen <kylemtnguyen@gmail.com>
2025-11-15 22:21:37 -0700

    Organizer can now view all types of participant statuses

---
+++ SRC/app/src/main/java/com/example/quokkapuffevents/controller/OrganizerViewParticipantsFragment.java
@@ -0,0 +1,246 @@
+package com.example.quokkapuffevents.controller;
+
+import static android.view.View.GONE;
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.quokkapuffevents.R;
+import com.example.quokkapuffevents.model.Database;
+import com.example.quokkapuffevents.model.Event;
+import com.example.quokkapuffevents.model.User;
+import com.example.quokkapuffevents.view.AdminUserFragAdapter;
+import com.example.quokkapuffevents.view.OrgViewParticipantsFragAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class OrganizerViewParticipantsFragment extends Fragment {
+
+//    TYPES of filters: "Invited", "Waiting", "Canceled"
+
+    private Database db;
+    private Event event;
+
+    private TextView viewType;
+    private TextView waitingListAmt;
+
+    private Button canceledBtn;
+    private Button waitlistBtn;
+    private Button chosenBtn;
+    private Button finalParticipantsBtn;
+    private Button redrawBtn;
+    private Button backBtn;
+
+    ListView listView;
+    private OrgViewParticipantsFragAdapter adapter;
+    private ArrayList<User> userList = new ArrayList<>();
+
+    public void SetEvent(Event event) {
+        this.event = event;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View userFragmentView = inflater.inflate(R.layout.organizer_view_participants, container, false);
+
+        db = Database.getInstance();
+
+        listView = userFragmentView.findViewById(R.id.orgParticipantListView);
+
+        adapter = new OrgViewParticipantsFragAdapter(requireContext(), userList);
+        listView.setAdapter(adapter);
+
+        Map<String, String> eventUsers = event.getEventUsers();
+        userList.clear();
+        AtomicInteger pending = new AtomicInteger(0);
+
+        for (Map.Entry<String, String> entry : eventUsers.entrySet()) {
+
+            if (entry.getValue().equals("Canceled")) {
+
+                pending.incrementAndGet();
+
+                db.GetUser(entry.getKey(), user -> {
+
+                    if (user != null) {
+                        userList.add(user);
+                    }
+
+                    if (pending.decrementAndGet() == 0) {
+
+                        if (userList.isEmpty()) {
+                            System.out.println("USERLIST IS EMPTY");
+                        } else {
+                            for (User u : userList) {
+                                System.out.println(u.getFirstName());
+                            }
+
+                        }
+                        adapter.notifyDataSetChanged();
+
+
+                    }
+
+                });
+
+            }
+        }
+
+
+
+
+        return userFragmentView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initialize(view);
+        SetUpListeners(view);
+    }
+
+    public void initialize(View view) {
+
+
+        viewType = view.findViewById(R.id.viewParticipantsTypeText);
+        waitingListAmt = view.findViewById(R.id.waitingListAmt);
+//        Buttons
+        canceledBtn = view.findViewById(R.id.viewCanceledParticipantsBtn);
+        waitlistBtn = view.findViewById(R.id.viewWaitingParticipantsBtn);
+        chosenBtn = view.findViewById(R.id.viewChosenParticipantsBtn);
+        redrawBtn = view.findViewById(R.id.orgRedrawEntrantBtn);
+        backBtn = view.findViewById(R.id.orgViewParticipantsBackToDashboardBtn);
+        finalParticipantsBtn = view.findViewById(R.id.viewFinalParticipantsBtn);
+
+    }
+
+    public void SetUpListeners(View view) {
+
+        canceledBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeViewType("Canceled", view);
+            }
+        });
+
+        waitlistBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeViewType("Waiting", view);
+            }
+        });
+
+        chosenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeViewType("Invited", view);
+            }
+        });
+
+        finalParticipantsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeViewType("Enrolled", view);
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OrganizerEventDetails orgFrag = new OrganizerEventDetails();
+                orgFrag.SetEvent(event);
+                ((DashboardActivity) getActivity()).replaceFragment(orgFrag);
+            }
+        });
+
+
+    }
+
+    public void changeViewType(String filterType, View view) {
+
+        Map<String, String> eventUsers = event.getEventUsers();
+        userList.clear();
+        AtomicInteger pending = new AtomicInteger(0);
+
+        for (Map.Entry<String, String> entry : eventUsers.entrySet()) {
+
+            if (entry.getValue().equals(filterType)) {
+//                Async stuff #annoying
+                pending.incrementAndGet();
+
+                db.GetUser(entry.getKey(), user -> {
+//                    Gett user and add it to the userList
+
+                    if (user != null) {
+                        userList.add(user);
+                    }
+
+                    if (pending.decrementAndGet() == 0) {
+
+                        if (userList.isEmpty()) {
+                            System.out.println("USERLIST IS EMPTY");
+                        } else {
+                            for (User u : userList) {
+                                System.out.println(u.getFirstName());
+                            }
+
+                        }
+
+                        adapter.notifyDataSetChanged();
+
+                        //            If waiting, view the amount.
+                        if(filterType.equals("Waiting")) {
+
+                            view.findViewById(R.id.amountWaitingListContainer).setVisibility(VISIBLE);
+                            view.findViewById(R.id.redrawContainer).setVisibility(GONE);
+                            waitingListAmt.setText(String.valueOf(userList.size()));
+
+                        }
+
+
+                    }
+
+                });
+
+            }
+        }
+
+        if (filterType.equals("Canceled")) {
+            view.findViewById(R.id.amountWaitingListContainer).setVisibility(GONE);
+            view.findViewById(R.id.redrawContainer).setVisibility(VISIBLE);
+
+        }
+        else {
+            view.findViewById(R.id.amountWaitingListContainer).setVisibility(GONE);
+            view.findViewById(R.id.redrawContainer).setVisibility(GONE);
+
+        }
+
+//      Show view type:
+        String temp = "View by type: " + filterType;
+        viewType.setText(temp);
+
+        adapter.notifyDataSetChanged();
+    }
+
+
+
+ }
--- SRC/app/src/main/java/com/example/quokkapuffevents/model/Database.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/model/Database.java
@@ -357,10 +357,21 @@ public class Database {
          * Returns an ArrayList that holds all of the users that have signed up for this event
          */
         //List of all users in the event
-        List usersInEvent = (List) event.getEventUsers().keySet();
+
+//       KYLE CHANGES vvvvv
+        List<String> usersInEvent = new ArrayList<>(event.getEventUsers().keySet());
+
+        if (usersInEvent.isEmpty()) {
+            listener.onSuccess(new ArrayList<>());
+            return;
+        }
+
+        //KYLE CHANGES ^^^
 
         //Collects the data for every user with an id in the above list
-        usersRef.whereIn("id", (List) usersInEvent).get()
+
+
+        usersRef.whereIn("id", usersInEvent).get() // edited it as well -- KYLE
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
                         ArrayList<User> users = new ArrayList<>();
