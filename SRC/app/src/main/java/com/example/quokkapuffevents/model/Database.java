d98bf95c6955cf6e0aa41838a7c8597aebf41a10
Aaron Bedard <abedard2005@gmail.com>
2025-11-19 20:00:56 -0700

    Notification Channel Created, moving to in app notifications

--- SRC/app/src/main/java/com/example/quokkapuffevents/model/Database.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/model/Database.java
@@ -1,11 +1,15 @@
 package com.example.quokkapuffevents.model;
 
+import static androidx.core.content.ContextCompat.checkSelfPermission;
 import static androidx.core.content.ContextCompat.getSystemService;
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -14,6 +18,7 @@ import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import com.example.quokkapuffevents.R;
 
@@ -39,7 +44,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
-import java.util.concurrent.CountDownLatch;
 
 
 public class Database {
@@ -166,24 +170,7 @@ public class Database {
 
     String CHANNEL_ID = "notificationChannelID";
 
-
-    private void createNotificationChannel(Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Source - https://stackoverflow.com/a
-            // Posted by k1r4n
-            // Retrieved 2025-11-19, License - CC BY-SA 4.0
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-            CharSequence name = "notificationChannel";
-            String description = "This is a notification channel";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-
-    public Notif CreateNotification(Integer type, String recipient, String originEvent, String originUser, String message, Context context){
+    public Notif CreateNotification(Integer type, String recipient, String originEvent, String originUser, String message){
         /**
          * Creates a new notification and saves the new notif data to the database
          * @param type
@@ -203,12 +190,17 @@ public class Database {
         Notif newNotif = new Notif(id, type, recipient, originEvent, originUser, message);
         notifsRef.document(id).set(newNotif);
 
-        createNotificationChannel(context);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.mipmap.app_icon)
-                .setContentTitle("Quokka Puffs Events")
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        if(checkSelfPermission(getApplicationContext(), android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getApplicationContext(), new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 1);
+        }
+
+        NotificationManager notifManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Notification Channel", NotificationManager.IMPORTANCE_HIGH);
+            notifManager.createNotificationChannel(channel);
+        }
+
         return(newNotif);
     }
     public void UploadImageToDatabase(Bitmap bitmap, OnSuccessListener<String> listener){
