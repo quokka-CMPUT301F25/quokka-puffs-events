937d3b113dd548f092c0acb0ecd3b394597e5d74
SABussiere <spencerbussiere2004@gmail.com>
2025-11-24 17:03:11 -0700

    Updating Test Cases

--- SRC/app/src/androidTest/java/com/example/quokkapuffevents/EntrantTestCases.java
+++ SRC/app/src/androidTest/java/com/example/quokkapuffevents/EntrantTestCases.java
@@ -328,16 +328,6 @@ public class EntrantTestCases {
     }
 
     /**
-     * User Story US 01.04.02 test case
-     */
-    // TODO: FINISH THE TEST
-    @Test
-    public void TestReceivingLostNotification() {
-        User entrant = accessEntrantDashboard();
-        db.DeleteUser(entrant);
-    }
-
-    /**
      * User Story US 01.02.02 test case
      */
     @Test
@@ -541,7 +531,7 @@ public class EntrantTestCases {
     }
 
     /**
-     * User Story US 01.05.02 test case
+     * User Story US  test case
      */
     // TODO: FIX THIS TEST
     @Test
@@ -621,69 +611,6 @@ public class EntrantTestCases {
             db.DeleteEvent(event);
         } finally {
             db.DeleteUser(mockEntrant);
-            db.DeleteUser(mockOrg);
-            db.DeleteEvent(event);
-        }
-    }
-
-    /**
-     * User Story US 02.07.02 test case
-     */
-    // TODO: FIX THIS TEST
-    @Test
-    public void TestSendNotifToAllSelected() {
-        User mockOrg = db.CreateUser("TestDraw@email.com", 1, "AHHHH", "OrgTest", "John", "Test", "0");
-        Event event = db.CreateEvent("TestDraw", mockOrg.getId(), "This event is used to test if a entrant is sent a notif", 2, 2, new Date(), new Date());
-        User temp = accessEntrantDashboard();
-
-        User mockEntrant1 = db.CreateUser("MockTest", 0, temp.getHashPassword(), "mockTest1", "John", "Test", "0");
-        User mockEntrant2 = db.CreateUser("MockTest", 0, temp.getHashPassword(), "mockTest2", "John", "Test", "0");
-        try {
-            Thread.sleep(1500);
-
-            db.RegisterUserIntoEvent(event, mockEntrant1);
-            db.RegisterUserIntoEvent(event, mockEntrant2);
-
-            event.drawUsers(-1);
-            Thread.sleep(1500);
-
-            ActivityScenario.launch(LoginActivity.class);
-            onView(withId(R.id.login_email_address)).perform(typeText(mockEntrant1.getUserName()));
-            closeSoftKeyboard();
-            onView(withId(R.id.login_password)).perform(typeText("password"));
-            closeSoftKeyboard();
-            onView(withId(R.id.sign_in_button)).perform(click());
-            Thread.sleep(1500);
-            onView(withId(R.id.notifs_button)).perform(click());
-            Thread.sleep(1500);
-
-            //Testing to see if notification has appeared
-            onView(withText(R.string.winner_header)).check(matches(isDisplayed()));
-
-            ActivityScenario.launch(LoginActivity.class);
-            onView(withId(R.id.login_email_address)).perform(typeText(mockEntrant2.getUserName()));
-            closeSoftKeyboard();
-            onView(withId(R.id.login_password)).perform(typeText("password"));
-            closeSoftKeyboard();
-            onView(withId(R.id.sign_in_button)).perform(click());
-            Thread.sleep(1500);
-            onView(withId(R.id.notifs_button)).perform(click());
-            Thread.sleep(1500);
-
-            //Testing to see if notification has appeared
-            onView(withText(R.string.winner_header)).check(matches(isDisplayed()));
-
-
-        } catch (InterruptedException e) {
-            db.DeleteUser(mockEntrant1);
-            db.DeleteUser(mockEntrant2);
-            db.DeleteUser(temp);
-            db.DeleteUser(mockOrg);
-            db.DeleteEvent(event);
-        } finally {
-            db.DeleteUser(mockEntrant1);
-            db.DeleteUser(mockEntrant2);
-            db.DeleteUser(temp);
             db.DeleteUser(mockOrg);
             db.DeleteEvent(event);
         }
