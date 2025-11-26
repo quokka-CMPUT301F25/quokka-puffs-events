a53dc5e4de685f9beb407555b34ab90feb75c3ee
Seth Close <seth@sethclose.au>
2025-11-26 15:26:32 -0700

    Created javadocs for Model and View classes

--- SRC/app/src/main/java/com/example/quokkapuffevents/model/Database.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/model/Database.java
@@ -19,7 +19,6 @@ import com.google.firebase.storage.UploadTask;
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
