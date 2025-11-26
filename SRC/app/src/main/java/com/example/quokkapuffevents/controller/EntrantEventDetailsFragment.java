ac333f8846b6c6441ebb9590eab61239c71e76f3
Seth Close <seth@sethclose.au>
2025-11-26 11:20:27 -0700

    Fixed the register button in the entrant event details page

--- SRC/app/src/main/java/com/example/quokkapuffevents/controller/EntrantEventDetailsFragment.java
+++ SRC/app/src/main/java/com/example/quokkapuffevents/controller/EntrantEventDetailsFragment.java
@@ -25,6 +25,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
+import java.util.Objects;
 
 public class EntrantEventDetailsFragment extends Fragment {
 
@@ -109,19 +110,6 @@ public class EntrantEventDetailsFragment extends Fragment {
 
 //        Display info to fragment
         orgEventNameText.setText(eventName);
-//        db.GetImage(event.getImageID(), new OnSuccessListener<Bitmap>() {
-//            @Override
-//            public void onSuccess(Bitmap bitmap) {
-//                if (getActivity() != null) {
-//                    getActivity().runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            eventImage.setImageBitmap(bitmap);
-//                        }
-//                    });
-//                }
-//            }
-//        });
         //Changed to improve
         db.GetImage(event.getImageID(), bitmap -> {
             eventImage.setImageBitmap(bitmap);
@@ -135,8 +123,13 @@ public class EntrantEventDetailsFragment extends Fragment {
         if (event.getEventDate().before(new Date())){
             entrantRegisterForEventBtn.setVisibility(INVISIBLE);
         }
-        if ((event.getNumPeopleWaiting() != -1) && (event.getNumPeopleWaiting() >= event.getMaxNumWaitlist())){
+        if ((event.getMaxNumWaitlist() != -1) && (event.getNumPeopleWaiting() >= event.getMaxNumWaitlist())){
             entrantRegisterForEventBtn.setVisibility(INVISIBLE);
+        }
+        if (event.getEventUsers().get(db.GetCurrentUserID()) != null){
+            if (!Objects.equals(event.getEventUsers().get(db.GetCurrentUserID()), "Cancelled")){
+                entrantRegisterForEventBtn.setVisibility(INVISIBLE);
+            }
         }
 
     }
